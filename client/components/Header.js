decbd0f76967f31bab18d78f250f353f5b2b23fa
JessicaNeary <Caneary@hotmail.com>
2017-04-17 14:20:43 +1200

    Header navigation links working

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import { categories } from '../../products.js'
+
 const Header = () => {
+  const links = categories.map(function (cat) {
+    return <Link key={cat.id} className='item' to={`category/${cat.id}`}>{cat.name}</Link>
+  })
   return (
     <div className='Header'>
       <div>
-        <img className='logo' src='https://placehold.it/100x100' />
+        <Link to='/'>
+          <img className='logo' src='https://placehold.it/100x100' />
+        </Link>
       </div>
       <div className='navigation'>
-        <Link className='item' to='/'>Tops</Link>
-        <Link className='item' to='/'>Bottoms</Link>
-        <Link className='item' to='/'>Accessories</Link>
-        <Link className='item' to='/'>Sale</Link>
+        {links}
+        <Link className='item' to='sale'>Sale</Link>
       </div>
     </div>
   )
